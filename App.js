7b1acd0ee89fb3073012d6856de4bcd470540b34
Nirmith Akash Anawaratne <68478214+AnawaratneMANA@users.noreply.github.com>
2021-07-18 23:59:35 +0530

    Calling Functions From Click Event And Single Line Text.

--- App.js
+++ App.js
@@ -4,9 +4,16 @@ import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   let x = 1;
+
+  const handlePressed = () =>  {
+    console.log("Pressed The Text")
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text numberOfLines={1} onPress = {handlePressed}>Open up App.js to
+        start working on
+        our app! This is a very long text, Want to see what happen</Text>
     </SafeAreaView>
   );
 }
